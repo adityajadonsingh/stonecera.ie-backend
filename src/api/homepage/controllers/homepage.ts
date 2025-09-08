761a0cc54ca1dd70131ada150f748d182e80c53e
Aditya Jadon <51057603+adityajadonsingh@users.noreply.github.com>
2025-09-08 15:00:14 +0530

    Update homepage.ts

--- src/api/homepage/controllers/homepage.ts
+++ src/api/homepage/controllers/homepage.ts
@@ -43,11 +43,16 @@ module.exports = createCoreController('api::homepage.homepage', ({ strapi }) => 
       robots: seo.robots,
     };
 
+    const getFullUrl = (url) => {
+      if (!url) return null;
+      return url.startsWith('http') ? url : `${process.env.MEDIA_URL}${url}`;
+    };
+
     const banners = (homepage.banners || []).map(banner => ({
       id: banner.id,
       title: banner.title,
       subtitle: banner.subtitle,
-      image: `${process.env.MEDIA_URL!}${banner.image?.url}` || null,
+      image: getFullUrl(banner.image?.url),
     }));
 
     const testimonials = (homepage.testimonials || []).map(testimonial => ({
